9ffa75dbe2b73c3d508f155e026c3ab64af7d4ea
StudioReboot <72804242+studioreboot@users.noreply.github.com>
2024-05-20 14:05:22 -0400

    Add files via upload

--- plugandplay/plugandplay.js
+++ plugandplay/plugandplay.js
@@ -1,6 +1,37 @@
 var PlugMorph;
 var SocketMorph;
+var PlugAndPlayMorph;
 
-function bind (aWorld) {
-    aWorld.
+PlugAndPlayMorph.prototype = new FrameMorph();
+PlugAndPlayMorph.prototype.constructor = PlugAndPlayMorph;
+PlugAndPlayMorph.uber = FrameMorph.prototype;
+
+function PlugAndPlayMorph () {
+    this.init();
 }
+
+PlugAndPlayMorph.prototype.init = function () {
+    PlugAndPlayMorph.uber.init.call(this);
+
+    this.color = WHITE;
+};
+
+PlugAndPlayMorph.prototype.makeTest = function () {
+    var txt;
+
+    txt = new TextMorph("there should be something here.");
+    txt.color = BLACK;
+    txt.setCenter(this.center());
+
+    this.add(txt);
+};
+
+PlugAndPlayMorph.prototype.openIn = function (aWorld) {
+    aWorld.add(this);
+    this.setExtent(aWorld.extent());
+    this.makeTest();
+}
+
+PlugAndPlayMorph.prototype.reactToWorldResize = function (aRect) {
+    this.setExtent(aRect.extent());
+};
